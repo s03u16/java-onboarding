823b6753b50be5e15460350e689bf83e76cd8b0b
s03u16 <0240j97e21@gmail.com>
2022-11-01 23:57:57 +0900

    commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,106 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+
+        // 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수
+
+        int p_l = pobi.get(0);
+        int p_r = pobi.get(1);
+        int c_l = crong.get(0);
+        int c_r = crong.get(1);
+
+
+        // 예외) 오른쪽 - 왼쪽 = 1 아닌 경우 & 왼쪽이 짝수인 경우 or 오른쪽이 홀수인 경우
+        if (p_r-p_l !=1 || c_r-c_l != 1 || c_l%2==0 || p_l%2==0) {
+            answer = -1;
+        }
+
+        else {
+            // pobi
+
+            List<Integer> p_l_list = new ArrayList<Integer>();
+            List<Integer> p_r_list = new ArrayList<Integer>();
+            while (p_l != 0) {
+                p_l_list.add(p_l % 10);
+                p_l /= 10;
+            }
+            while (p_r != 0) {
+                p_r_list.add(p_r % 10);
+                p_r /= 10;
+            }
+            int p_l_sum = 0;
+            for (int p : p_l_list) {
+                p_l_sum = p_l_sum + p;
+            }
+            int p_l_mul = 1;
+            for (int p : p_l_list) {
+                p_l_mul = p_l_mul * p;
+            }
+            int p_r_sum = 0;
+            for (int p : p_l_list) {
+                p_r_sum = p_r_sum + p;
+            }
+            int p_r_mul = 1;
+            for (int p : p_l_list) {
+                p_r_mul = p_r_mul * p;
+            }
+
+            int max_p_l = Math.max(p_l_sum, p_l_mul);
+            int max_p_r = Math.max(p_r_sum, p_r_mul);
+            int max_p = Math.max(max_p_l, max_p_r);
+
+
+            // crong
+
+            List<Integer> c_l_list = new ArrayList<Integer>();
+            List<Integer> c_r_list = new ArrayList<Integer>();
+            while (c_l != 0) {
+                c_l_list.add(c_l % 10);
+                c_l /= 10;
+            }
+            while (c_r != 0) {
+                c_r_list.add(c_r % 10);
+                c_r /= 10;
+            }
+            int c_l_sum = 0;
+            for (int c : c_l_list) {
+                c_l_sum = c_l_sum + c;
+            }
+            int c_l_mul = 1;
+            for (int c : c_l_list) {
+                c_l_mul = c_l_mul * c;
+            }
+            int c_r_sum = 0;
+            for (int c : c_l_list) {
+                c_r_sum = c_r_sum + c;
+            }
+            int c_r_mul = 1;
+            for (int c : c_l_list) {
+                c_r_mul = c_r_mul * c;
+            }
+
+
+            int max_c_l = Math.max(c_l_sum, c_l_mul);
+            int max_c_r = Math.max(c_r_sum, c_r_mul);
+            int max_c = Math.max(max_c_l, max_c_r);
+
+            if (max_p > max_c) {
+                answer = 1;
+            }
+            else if (max_p < max_c) {
+                answer = 2;
+            }
+            else {
+                answer = 0;
+            }
+
+
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        //List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 각 몇 개
+        int[] money_list = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int m: money_list) {
+            if (money >= m) {
+                answer.add(money / m);
+            } else {
+                answer.add(0);
+            }
+            money = money - m * (money / m);
+        }
         return answer;
     }
 }
